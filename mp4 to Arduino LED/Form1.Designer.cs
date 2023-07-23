797e5c36fe9457dea64bc333a1eab717ce5ca42c
rklavin <rklav12@hotmail.com>
2023-07-23 17:03:32 -0400

    Add conversion to Arduino animation file

--- mp4 to Arduino LED/Form1.Designer.cs
+++ mp4 to Arduino LED/Form1.Designer.cs
@@ -42,12 +42,21 @@
             imageBox1 = new Emgu.CV.UI.ImageBox();
             imageBox2 = new Emgu.CV.UI.ImageBox();
             button4 = new Button();
+            trackBar1 = new TrackBar();
+            button5 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label5 = new Label();
+            label6 = new Label();
+            numericUpDown2 = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)imageBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)imageBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -81,12 +90,13 @@
             // button2
             // 
             button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            button2.Location = new Point(12, 340);
+            button2.Location = new Point(12, 356);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 8;
             button2.Text = "Convert";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label4
             // 
@@ -179,7 +189,7 @@
             // button4
             // 
             button4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            button4.Location = new Point(206, 340);
+            button4.Location = new Point(219, 356);
             button4.Name = "button4";
             button4.Size = new Size(120, 23);
             button4.TabIndex = 13;
@@ -187,11 +197,71 @@
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(358, 344);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(310, 45);
+            trackBar1.TabIndex = 14;
+            trackBar1.TickStyle = TickStyle.None;
+            trackBar1.ValueChanged += trackBar1_ValueChanged;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button5.Location = new Point(93, 356);
+            button5.Name = "button5";
+            button5.Size = new Size(120, 23);
+            button5.TabIndex = 15;
+            button5.Text = "Previous Frame";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(12, 328);
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(75, 23);
+            numericUpDown1.TabIndex = 16;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(93, 330);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 15);
+            label5.TabIndex = 17;
+            label5.Text = "n Frames";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(249, 330);
+            label6.Name = "label6";
+            label6.Size = new Size(90, 15);
+            label6.TabIndex = 19;
+            label6.Text = "Drop nth Frame";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(168, 328);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(75, 23);
+            numericUpDown2.TabIndex = 18;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(680, 375);
+            ClientSize = new Size(680, 391);
+            Controls.Add(label6);
+            Controls.Add(numericUpDown2);
+            Controls.Add(label5);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button5);
+            Controls.Add(trackBar1);
             Controls.Add(button4);
             Controls.Add(splitContainer1);
             Controls.Add(label4);
@@ -201,7 +271,7 @@
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(button1);
-            MinimumSize = new Size(696, 414);
+            MinimumSize = new Size(696, 430);
             Name = "Form1";
             Text = "Form1";
             splitContainer1.Panel1.ResumeLayout(false);
@@ -212,6 +282,9 @@
             splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)imageBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)imageBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -231,5 +304,11 @@
         private Button button4;
         private Emgu.CV.UI.ImageBox imageBox1;
         private Emgu.CV.UI.ImageBox imageBox2;
+        private TrackBar trackBar1;
+        private Button button5;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+        private Label label6;
+        private NumericUpDown numericUpDown2;
     }
 }
--- mp4 to Arduino LED/Form1.cs
+++ mp4 to Arduino LED/Form1.cs
@@ -30,7 +30,13 @@ namespace mp4_to_Arduino_LED
                 textBox1.Text = openFileDialog.FileName;
                 videoCapture = new VideoCapture(openFileDialog.FileName);
                 button4_Click(sender, e);
+                updateSliderRange();
             }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create output file
+            convertMatToArduino((int)numericUpDown1.Value, (int)numericUpDown2.Value);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -52,6 +58,48 @@ namespace mp4_to_Arduino_LED
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (trackBar1.Value < trackBar1.Maximum)
+            {
+                trackBar1.Value += 1;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (trackBar1.Value > trackBar1.Minimum)
+            {
+                trackBar1.Value -= 1;
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (videoCapture != null)
+            {
+                videoCapture.Set(CapProp.PosFrames, trackBar1.Value - 1);
+                updatePreviews();
+            }
+        }
+
+        private Mat convertMatToLED(Mat mat, int height, int width)
+        {
+            Mat output = new Mat(height, width, Emgu.CV.CvEnum.DepthType.Default, 3);
+            CvInvoke.Resize(mat, output, new Size(12, 8), 1, 1, Inter.NearestExact);
+            CvInvoke.Threshold(output, output, 127, 255, ThresholdType.Binary);
+
+            return output;
+        }
+
+        private void updateSliderRange()
+        {
+            if (videoCapture != null)
+            {
+                trackBar1.SetRange(0, (int)videoCapture.Get(CapProp.FrameCount));
+            }
+        }
+
+        private void updatePreviews()
+        {
             if (videoCapture != null)
             {
                 using (Mat mat = videoCapture.QueryFrame())
@@ -60,26 +108,101 @@ namespace mp4_to_Arduino_LED
                     imageBox1.Image = mat;
 
                     // Make and update output preview
-                    Mat output = convertMatToLED(mat);
+                    Mat output = convertMatToLED(mat, 8, 12);
                     imageBox2.Image = output;
                     output.Dispose();
                 }
             }
         }
 
-        private Mat convertMatToLED(Mat mat)
+        private void convertMatToArduino(int nFrames, int dropFrame)
         {
-            int width = mat.Size.Width;
-            int height = mat.Size.Height;
+            // No output file selected
+            if (textBox2 == null) return;
 
-            int pixelWidth = width / 12;
-            int pixelHeight = height / 8;
+            // Can't grab less than 1 frame
+            if (nFrames <= 0) return;
 
-            Mat output = new Mat(8, 12, Emgu.CV.CvEnum.DepthType.Default, 3);
-            CvInvoke.Resize(mat, output, new Size(12, 8), 1, 1, Inter.NearestExact);
-            CvInvoke.Threshold(output, output, 127, 255, ThresholdType.Binary);
+            if (videoCapture != null)
+            {
+                // Create and open output file
+                StreamWriter output = new StreamWriter(textBox2.Text);
 
-            return output;
+                // Write first line of animation file
+                output.WriteLine("const uint32_t animation[][4] = {");
+
+                // Get frame count
+                int frames = (int)videoCapture.Get(CapProp.FrameCount);
+                double dropFPS = videoCapture.Get(CapProp.Fps);
+                if (dropFrame > 0)
+                {
+                    dropFPS = dropFPS - (videoCapture.Get(CapProp.Fps) / dropFrame);
+                }
+                double frameTime = ((1 / dropFPS) * 1000 * nFrames) - 1;
+
+                // Reset video to start
+                videoCapture.Set(CapProp.PosFrames, 0);
+
+                for (int i = 0; i < frames; i += nFrames)
+                {
+                    if (nFrames > 1)
+                    {
+                        // Setting the property is much slower
+                        videoCapture.Set(CapProp.PosFrames, i);
+                    }
+
+                    Mat mat = videoCapture.QueryFrame();
+
+                    // Drop frame
+                    if ((dropFrame) > 0 && (i % dropFrame) == 0)
+                    {
+                        mat.Dispose();
+                        continue;
+                    }
+
+                    Mat matLED = convertMatToLED(mat, 8, 12);
+                    mat.Dispose();
+
+                    // Allow iterating through pixel data
+                    Image<Bgr, Byte> img = matLED.ToImage<Bgr, Byte>();
+                    matLED.Dispose();
+
+                    // Write LED representation
+                    output.WriteLine("\t{");
+
+                    string leds = "";
+                    for (int j = 0; j < img.Rows; j += 1)
+                    {
+                        int line = 0;
+                        for (int k = 0; k < img.Cols; k += 1)
+                        {
+                            line += img.Data[j, k, 0] > 0 ? 1 : 0;
+                            line <<= 1;
+                        }
+                        line >>= 1;
+                        line &= 0xFFF;
+                        leds += line.ToString("X3");
+                    }
+
+                    output.WriteLine("\t\t0x" + leds.Substring(0, 8) + ",");
+                    output.WriteLine("\t\t0x" + leds.Substring(8, 8) + ",");
+                    output.WriteLine("\t\t0x" + leds.Substring(16, 8) + ",");
+
+                    output.WriteLine("\t\t" + frameTime);
+
+                    output.Write("\t}");
+                    if (i < frames - 1)
+                    {
+                        output.Write(",");
+                    }
+                    output.WriteLine("");
+                }
+
+                // Write last line of animation file
+                output.WriteLine("};");
+
+                output.Close();
+            }
         }
     }
 }
